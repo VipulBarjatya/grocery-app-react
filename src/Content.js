060ec38619789a4d5aa6689f261b5b68a2eab9df
Vipul <vipulbarjatya@gmail.com>
2022-11-30 11:09:23 +0530

    click Events

--- src/Content.js
+++ src/Content.js
@@ -7,9 +7,34 @@ const Content = () => {
     return names[index];
   };
 
+  const handleClick = () => {
+    console.log("You clicked");
+  };
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
+      <button onClick={handleClick}>Click</button>
+      <button
+        onClick={() => {
+          handleClick2("Vipul");
+        }}
+      >
+        Click
+      </button>
+      <button
+        onClick={(e) => {
+          handleClick3(e);
+        }}
+      >
+        Click
+      </button>
     </main>
   );
 };
